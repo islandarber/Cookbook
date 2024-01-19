db9525c5be6044b582b0a85c36e4946a4de0c07c
Christina Vekri <christina.vek@gmail.com>
2024-01-19 15:12:55 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import NavBar from './views/Navbar';
+import HomePage from './views/HomePage';
+import { Routes, Route } from 'react-router-dom';
+import RecipeCard from './views/RecipeCard';
+import Footer from './views/Footer';
+import Gallery from './views/Gallery';
+
+function App() {
+
+
+  return (
+    <>
+    <div className='container'>
+      <NavBar />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/:id' element={<RecipeCard />} />
+          <Route path='/gallery' element={<Gallery />} />
+        </Routes>
+    </div>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
